8b63f58cd22f67d7f0ce3ea31560dc8deff8b35f
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-22 02:18:45 +0600

    getting real time data

--- server/index.js
+++ server/index.js
@@ -12,12 +12,22 @@ const io = new Server(expressServer);
 io.on("connection", function (socket) {
   console.log("New User Connected");
 
+  // send data continuously
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.send(time);
+  },500);
+
+  // send data after a time
+  setTimeout(() => {
+    socket.send("Welcome To server Side to Client");
+  }, 3000);
+
   socket.on("disconnect", function () {
     console.log("User Disconnected!");
   });
 });
-
-
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./index.html"));
