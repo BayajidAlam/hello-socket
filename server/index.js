046ac9f1a16651e769d6412452cdaaa29a057bf9
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-22 14:09:09 +0600

    broadcasting added

--- server/index.js
+++ server/index.js
@@ -9,35 +9,48 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
+// io.on("connection", function (socket) {
+//   console.log("New User Connected");
+
+//   // send data after a time
+//   setTimeout(() => {
+//     socket.send("Welcome To server Side to Client");
+//   }, 3000);
+
+//   // send data continuously
+//   setInterval(() => {
+//     let date = new Date();
+//     let time = date.getTime();
+//     socket.send(time);
+//   }, 10);
+
+//   // create a custom event
+//   setInterval(() => {
+//     let date = new Date();
+//     let time = date.getTime();
+//     socket.emit("myEvent", time);
+//   }, 10);
+
+//   // receive data from client
+//   socket.on("MyClientEvent", function (data) {
+//     console.log(data);
+//   });
+
+//   socket.on("disconnect", function () {
+//     console.log("User Disconnected!");
+//   });
+// });
+
+//send broadcasting data
 io.on("connection", function (socket) {
+  io.sockets.emit("MyBroadCast", "Hello Everyone");
   console.log("New User Connected");
+}),
 
-  // send data after a time
-  setTimeout(() => {
-    socket.send("Welcome To server Side to Client");
-  }, 3000);
 
-  // send data continuously
-  setInterval(() => {
-    let date = new Date();
-    let time = date.getTime();
-    socket.send(time);
-  }, 10);
-
-  // create a custom event
-  setInterval(() => {
-    let date = new Date();
-    let time = date.getTime();
-    socket.emit("myEvent", time);
-  }, 10);
-
-  socket.on("disconnect", function () {
-    console.log("User Disconnected!");
-  });
-});
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./index.html"));
+    res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 expressServer.listen(port, function () {
