ec568e35d9e271591dca65c808e3eb26ed102ebd
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-22 14:22:50 +0600

    working name space

--- server/index.js
+++ server/index.js
@@ -9,37 +9,37 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-// io.on("connection", function (socket) {
-//   console.log("New User Connected");
+io.on("connection", function (socket) {
+  console.log("New User Connected");
 
-//   // send data after a time
-//   setTimeout(() => {
-//     socket.send("Welcome To server Side to Client");
-//   }, 3000);
+  // send data after a time
+  setTimeout(() => {
+    socket.send("Welcome To server Side to Client");
+  }, 3000);
 
-//   // send data continuously
-//   setInterval(() => {
-//     let date = new Date();
-//     let time = date.getTime();
-//     socket.send(time);
-//   }, 10);
+  // send data continuously
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.send(time);
+  }, 10);
 
-//   // create a custom event
-//   setInterval(() => {
-//     let date = new Date();
-//     let time = date.getTime();
-//     socket.emit("myEvent", time);
-//   }, 10);
+  // create a custom event
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.emit("myEvent", time);
+  }, 10);
 
-//   // receive data from client
-//   socket.on("MyClientEvent", function (data) {
-//     console.log(data);
-//   });
+  // receive data from client
+  socket.on("MyClientEvent", function (data) {
+    console.log(data);
+  });
 
-//   socket.on("disconnect", function () {
-//     console.log("User Disconnected!");
-//   });
-// });
+  socket.on("disconnect", function () {
+    console.log("User Disconnected!");
+  });
+});
 
 //send broadcasting data
 io.on("connection", function (socket) {
@@ -47,10 +47,19 @@ io.on("connection", function (socket) {
   console.log("New User Connected");
 }),
 
+// namespace
+// let buyNsp = io.of("/buy");
+// buyNsp.on("connection", function (socket) {
+//   buyNsp.emit("MyBroadCast", "Hello Buy World");
+// });
 
+// let sellNsp = io.of("/sell");
+// sellNsp.on("connection", function (socket) {
+//   sellNsp.emit("MyBroadCast", "Hello Sell World");
+// });
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 expressServer.listen(port, function () {
