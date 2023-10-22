6d89243a02083447ec52a8c08e59083aeb067f03
BayajidAlam <bayajidalam2001@gmail.com>
2023-10-22 18:52:49 +0600

    room and size covered

--- server/index.js
+++ server/index.js
@@ -9,60 +9,61 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-// io.on("connection", function (socket) {
-//   console.log("New User Connected");
+io.on("connection", function (socket) {
+  console.log("New User Connected");
 
-//   // send data after a time
-//   setTimeout(() => {
-//     socket.send("Welcome To server Side to Client");
-//   }, 3000);
+// send data after a time
+  setTimeout(() => {
+    socket.send("Welcome To server Side to Client");
+  }, 3000);
 
-//   // send data continuously
-//   setInterval(() => {
-//     let date = new Date();
-//     let time = date.getTime();
-//     socket.send(time);
-//   }, 10);
+// send data continuously
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.send(time);
+  }, 10);
 
-//   // create a custom event
-//   setInterval(() => {
-//     let date = new Date();
-//     let time = date.getTime();
-//     socket.emit("myEvent", time);
-//   }, 10);
+ // create a custom event
+  setInterval(() => {
+    let date = new Date();
+    let time = date.getTime();
+    socket.emit("myEvent", time);
+  }, 10);
 
-//   // receive data from client
-//   socket.on("MyClientEvent", function (data) {
-//     console.log(data);
-//   });
+ // receive data from client
+  socket.on("MyClientEvent", function (data) {
+    console.log(data);
+  });
 
-//   socket.on("disconnect", function () {
-//     console.log("User Disconnected!");
-//   });
-// });
+  socket.on("disconnect", function () {
+    console.log("User Disconnected!");
+  });
+});
 
 //send broadcasting data
-// io.on("connection", function (socket) {
-//   io.sockets.emit("MyBroadCast", "Hello Everyone");
-//   console.log("New User Connected");
-// }),
+io.on("connection", function (socket) {
+  io.sockets.emit("MyBroadCast", "Hello Everyone");
+  console.log("New User Connected");
+}),
 
 // namespace
-// let buyNsp = io.of("/buy");
-// buyNsp.on("connection", function (socket) {
-//   buyNsp.emit("MyBroadCast", "Hello Buy World");
-// });
+let buyNsp = io.of("/buy");
+buyNsp.on("connection", function (socket) {
+  buyNsp.emit("MyBroadCast", "Hello Buy World");
+});
 
-// let sellNsp = io.of("/sell");
-// sellNsp.on("connection", function (socket) {
-//   sellNsp.emit("MyBroadCast", "Hello Sell World");
-// });
+let sellNsp = io.of("/sell");
+sellNsp.on("connection", function (socket) {
+  sellNsp.emit("MyBroadCast", "Hello Sell World");
+});
 
 // room
 io.on("connection", function (socket) {
   // kitchen room 
   socket.join("kitchen-room");
-  io.sockets.in("kitchen-room").emit("cooking", "Fried Rice Cooking");
+  let sizeOfKitchen = io.sockets.adapter.rooms.get('kitchen-room').size;
+  io.sockets.in("kitchen-room").emit("cooking", "Fried Rice Cooking = " + sizeOfKitchen);
 
   // bed room 
   socket.join("bed-room");
